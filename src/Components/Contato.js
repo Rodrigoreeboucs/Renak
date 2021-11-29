72308ffb6692fd59a7d2fcb3254a231e80ee0b43
rodrigo rebouças <reboucasrodrig@gmail.com>
2021-11-29 15:14:46 -0300

    commit

---
+++ src/Components/Contato.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styles from './Contato.module.css'
+import foto from '../img/contato.jpg'
+
+function Contato() {
+    return (
+        <section className={styles.contato + ' animeLeft'}>
+            <img src={foto} alt="Máquina de escrever" />
+            <div>
+            <h1>Entre em Contato</h1>
+            <ul className={styles.dados}>
+                <li>andre@origamid.com</li>
+                <li>9999-9999</li>
+                <li>Ali perto, 999</li>
+            </ul>
+            </div>
+        </section>
+    )
+}
+
+export default Contato
+
---
+++ src/Components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <h1>
+                Alguns direitos reservados.
+            </h1>
+        </footer>
+    )
+}
+
+export default Footer
